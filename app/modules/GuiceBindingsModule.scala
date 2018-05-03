b8c32f859ea6b1e4769f1aac825eb9c0fbf6cca1
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-03 22:03:22 +1000

    Terminating the Play application if initialization fails

--- app/modules/GuiceBindingsModule.scala
+++ app/modules/GuiceBindingsModule.scala
@@ -4,42 +4,56 @@ import com.google.inject.AbstractModule
 import com.outworkers.phantom.connectors.{CassandraConnection, ContactPoints}
 import constants.{DefaultConfigs, EnvVariableNames}
 import daos.DaoType._
-import daos.phantom.PhantomTagDao
+import daos.phantom.{PhantomProductDao, PhantomTagDao}
 import daos.slick.SlickTagDao
-import daos.{DaoType, TagDao}
+import daos.{DaoType, ProductDao, TagDao}
 import utils.ConfigUtils._
 
 import scala.util.Try
+import scala.util.control.NonFatal
 
 class GuiceBindingsModule extends AbstractModule
 {
   import GuiceBindingsModule._
 
   override def configure(): Unit =
+    bindings().recover {
+      case NonFatal(throwable) => terminate(throwable)
+    }
+
+  private def bindings(): Try[Unit] =
   {
     implicit val environmentVariables: EnvironmentVariables = EnvironmentVariables(sys.env)
 
     bind(classOf[EnvironmentVariables]).toInstance(environmentVariables)
-    bindDaoType()
+
+    for {
+      _ <- bindDaoType()
+    }
+    yield (): Unit
   }
 
-  private def bindDaoType()(implicit environmentVariables: EnvironmentVariables) =
+  private def bindDaoType()(implicit environmentVariables: EnvironmentVariables): Try[_] =
     getEnvValue(EnvVariableNames.DAO_TYPE) match {
       case DaoType(Slick) => bindSlickDao()
       case DaoType(Cassandra) => bindCassandraDao()
       case _ => bindSlickDao()
     }
 
-  private def bindSlickDao() =
-  {
-    bind(classOf[TagDao]).to(classOf[SlickTagDao])
-  }
+  private def bindSlickDao(): Try[_] =
+    Try {
+      bind(classOf[TagDao]).to(classOf[SlickTagDao])
+    }
 
-  private def bindCassandraDao()(implicit environmentVariables: EnvironmentVariables) =
-  {
-    bind(classOf[CassandraConnection]).toInstance(cassandraConnection().get)
-    bind(classOf[TagDao]).to(classOf[PhantomTagDao]).asEagerSingleton()
-  }
+  private def bindCassandraDao()(implicit environmentVariables: EnvironmentVariables): Try[_] =
+    cassandraConnection()
+      .map {
+        connection => {
+          bind(classOf[CassandraConnection]).toInstance(connection)
+          bind(classOf[TagDao]).to(classOf[PhantomTagDao]).asEagerSingleton()
+          bind(classOf[ProductDao]).to(classOf[PhantomProductDao]).asEagerSingleton()
+        }
+      }
 }
 
 object GuiceBindingsModule
